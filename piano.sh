f83e6bd92970b2231bea65bfc8e19cb465d57792
DiegoMolero <diego.molero@alu.uclm.es>
2018-03-07 10:38:27 +0100

    Controls if a MIDI wire is connected

--- piano.sh
+++ piano.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 OUTPUT="$(amidi -l)" || sudo apt-get install amidi
-echo "${OUTPUT}"
+echo "Output: ${OUTPUT}"
+if [[ $OUTPUT = *"MIDI"* ]]; then
+	echo "$OUTPUT"
+else
+	echo "Connect the MIDI wire from the piano to the Raspberry"
+fi
