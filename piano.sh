ca2813df671feab41d688f4bd2bc93607fee3114
DiegoMolero <diego.molero@alu.uclm.es>
2018-03-07 11:59:01 +0100

    server now recives arguments HOST and PORT for the data from amidi

--- piano.sh
+++ piano.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 OUTPUT="$(amidi -l)" || sudo apt-get install amidi
 if [[ $OUTPUT = *"MIDI"* ]]; then
-	echo "$OUTPUT"
+	var_random = $((RANDOM%1100+1000))
+	echo var_random
 else
 	echo "Connect the MIDI wire from the piano to the Raspberry"
 fi
--- recive.py
+++ recive.py
@@ -5,40 +5,23 @@
 #
 import sys,socket
 
-HOST = 'localhost'   # use '' to expose to all networks
-PORT = 12345
-
-def main():
-	inputfile = ''
-	outputfile = ''
-	try:
-		if(len(sys.argv) != 3):
-			print 'Sintex error: test.py <host> <port>'
-			sys.exit(2)
-		HOST = str(sys.argv[1])
-		PORT = int(sys.argv[2])
-		print HOST
-		print PORT
-      
-      
-	  
-if __name__ == "__main__":
-	main()
-'''
+# /-- network ---
 def incoming(host, port):
-  """Open specified port and return file-like object"""
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  # set SOL_SOCKET.SO_REUSEADDR=1 to reuse the socket if
-  # needed later without waiting for timeout (after it is
-  # closed, for example)
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   sock.bind((host, port))
   sock.listen(0)   # do not queue connections
   request, addr = sock.accept()
   return request.makefile('r', 0)
-# /-- network ---
 
-
-for line in incoming(HOST, PORT):
-  print line,
-'''
+def main(argv):
+    if(len(argv) != 3):
+        print ('Sintex error: test.py <host> <port>')
+        sys.exit(2)
+    host = str(argv[1])
+    port = int(argv[2])
+    for line in incoming(host, port):
+        print (line)
+        
+if __name__== "__main__":
+    main(sys.argv)
