9ec75d30ea672a1ffd35d85ec02da2f60ba6a393
DiegoMolero <diego.molero@alu.uclm.es>
2018-03-20 13:37:07 +0100

    recieving ip form hololens

--- recive_send_udp.py
+++ recive_send_udp.py
@@ -18,7 +18,7 @@ def UDPListener():
 	global hololens_ip
 	while True:
 		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-		hololens_ip = str(addr[0])
+		hololens_ip = str(data)
 		print (str(data.decode())+"From:"+hololens_ip)
 		
 
