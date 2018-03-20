42c43672633e2ab992f0086cc2b1d29e3bcf8ca4
DiegoMolero <diego.molero@alu.uclm.es>
2018-03-20 12:44:04 +0100

    allowing send piano data to hololens with udp

--- recive_send_udp.py
+++ recive_send_udp.py
@@ -10,30 +10,23 @@ UDP_PORT_LISTENER = 8000
 BUFFER_SIZE = 16  # Normally 1024, but we want fast response
 
 # /-- Hololens Server Network ---
-def sendData(data_piano):
-	if 'conn' in globals():
-		conn.send(data_piano.encode())  # echo
-		print('Data sent: '+data_piano)
 		
 def UDPListener():
 	print("Starting UDP Listener in "+gethostbyname("")+":"+str(UDP_PORT_LISTENER))
 	sock = socket(AF_INET,SOCK_DGRAM) # Internet,UDP
 	sock.bind(('', UDP_PORT_LISTENER))
+	global hololens_ip
 	while True:
 		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-		print (str(data.decode())+"From:"+addr)
+		hololens_ip = str(addr[1])
+		print (str(data.decode())+"From:"+hololens_ip)
 		
-def UDPSender():
-	print("Starting UDP Sender to port:"+str(UDP_PORT_SENDER))
-	sock_sender = socket(AF_INET,SOCK_DGRAM) # Internet,UDP
-	while True:
-		sock_sender.sendto(("hello").encode(), (HOLOLENS_IP, UDP_PORT_SENDER))
-		time.sleep(3)
 
 # /-- Local Network ---
 def startUDP(port):
 	print("Starting UDP Local Server, port:"+port)
-	sock = socket(AF_INET, SOCK_STREAM)
+	sock = socket(AF_INET, SOCK_STREAM)	# Localhost, UDP For Piano
+	sock_sender = socket(AF_INET,SOCK_DGRAM) # Internet,UDP For Hololens
 	sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 	sock.bind(('localhost', int(port)))
 	sock.listen(0)   # do not queue connections
@@ -41,8 +34,9 @@ def startUDP(port):
 	while 1:
 		data_piano = client.recv(BUFFER_SIZE)
 		print("Local data piano recieved:"+data_piano)
-		sendData(data_piano)
-	
+		if 'hololens_ip' in globals():
+			sendData(data_piano)
+			sock_sender.sendto(data_piano.encode(), (hololens_ip, UDP_PORT_SENDER))
 
 def main(argv):
 	if(len(argv) != 2):
